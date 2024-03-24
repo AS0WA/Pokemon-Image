78810777ddbbf8afe5443b70056e2ab6a85324b0
Antoni Siwiorek <146232484+AS0WA@users.noreply.github.com>
2024-03-24 04:37:05 +0100

    Update Pokemon.py

--- Pokemon.py
+++ Pokemon.py
@@ -3,6 +3,7 @@ from PIL import Image
 import PySimpleGUI as sg
 import os
 import re
+import json
 
 
 def pokemon_func(pokemon_name, graphic, shiny, who):
@@ -31,6 +32,7 @@ def pokemon_image_url(pokemon_name, graphic, shiny):
             f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower() and pokemon_name.replace(' ', '-')}"
         )
         pokemon = pokeapi.json()
+        print('pobieranie')
 
         if graphic == "Pixel Art" and shiny is True:
             url = pokemon["sprites"]["front_shiny"]
@@ -76,35 +78,34 @@ def pokemon_who(pokemon_identity):
 
 
 def pokemon_find(pokemon_name, graphic, shiny, who):
-    pokemon_pattern = f'[^ ]* {graphic}'
+    pokemon_pattern = f'{graphic}'
     if shiny:
         pokemon_pattern += ' shiny'
     if who:
         pokemon_pattern += ' who'
     if pokemon_name.isdigit():
-        pokemon_pattern = f'_-_{pokemon_name} {pokemon_pattern}.png'
+        pokemon_pattern = f'_-_{pokemon_name} [^ ]* {pokemon_pattern}.png'
     else:
-        pokemon_pattern = f' {pokemon_name} {pokemon_pattern}.png'
+        pokemon_pattern = f'_-_[0-9]+ {pokemon_name} {pokemon_pattern}.png'
     pokemons_list = [pokemon for pokemon in os.listdir('pokemons')]
     pokemon_identity = re.findall(pokemon_pattern, ' _-_'.join(pokemons_list))
     # pokemon1pokemon2 > pokemon1_-_pokemon2
 
     # if Pokémon 'who' image not exist, but Pokémon images exist make 'who' of it
-    if not pokemon_identity:
-        if who:
-            pokemon_pattern = f'[^ ]* {graphic}'
-            if shiny:
-                pokemon_pattern += ' shiny'
-            if pokemon_name.isdigit():
-                pokemon_pattern = f'_-_{pokemon_name} {pokemon_pattern}.png'
-            else:
-                pokemon_pattern = f' {pokemon_name} {pokemon_pattern}.png'
-            pokemons_list = [pokemon for pokemon in os.listdir('pokemons')]
-            pokemon_identity = re.findall(pokemon_pattern, ' _-_'.join(pokemons_list))
+    if not pokemon_identity and who:
+        pokemon_pattern = f'[^ ]* {graphic}'
+        if shiny:
+            pokemon_pattern += ' shiny'
+        if pokemon_name.isdigit():
+            pokemon_pattern = f'_-_{pokemon_name} {pokemon_pattern}.png'
+        else:
+            pokemon_pattern = f'_-_[0-9]+ {pokemon_name} {pokemon_pattern}.png'
+        pokemons_list = [pokemon for pokemon in os.listdir('pokemons')]
+        pokemon_identity = re.findall(pokemon_pattern, ' _-_'.join(pokemons_list))
 
-            if pokemon_identity:
-                pokemon_who(str(pokemon_identity)[5::][:-6])
-                pokemon_identity[0] = pokemon_identity[0][:-4] + ' who.png'
+        if pokemon_identity:
+            pokemon_who(str(pokemon_identity)[5::][:-6])
+            pokemon_identity[0] = pokemon_identity[0][:-4] + ' who.png'
 
 
     if not pokemon_identity:
@@ -115,14 +116,29 @@ def pokemon_find(pokemon_name, graphic, shiny, who):
     else:
         pokemon_data = []
         pokemon_identity = str(pokemon_identity)[5::][:-6]
-        pokemon_name = re.findall('^[0-9]* [^ ]*', str(pokemon_identity))[0]
-        pokemon_id = int(''.join(filter(lambda x: x.isdigit(), str(pokemon_name))))
-        pokemon_data.extend([[pokemon_name[len(str(pokemon_id)):], pokemon_id], pokemon_identity])
+        pokemon_rename = re.findall('^[0-9]* [^ ]*', str(pokemon_identity))[0]
+        pokemon_id = int(''.join(filter(lambda x: x.isdigit(), str(pokemon_rename))))
+        pokemon_data.extend([[pokemon_rename[len(str(pokemon_id))+1:], pokemon_id], pokemon_identity])
+
     return pokemon_data
 
 
+input_with = 20
+num_items_to_show = 5
+
+choices = []
+with open('pokemons/.pokemons.json', 'r') as pokemons_json:
+    pokemons = json.load(pokemons_json)
+    for pokemon_dict in pokemons:
+        for pokemon in pokemon_dict.values():
+            if pokemon.isdigit() is False:
+                choices.append(pokemon)
+
 layout = [
-    [sg.Text("Enter Pokemon Name or ID:"), sg.InputText(key="pokemon name")],
+    [sg.Text("Enter Pokemon Name or ID:"), sg.InputText(key="pokemon name", enable_events=True)],
+    [sg.Push(), sg.pin(sg.Col([[sg.Listbox(values=[], size=(47, num_items_to_show), enable_events=True, key='box',
+                                select_mode=sg.LISTBOX_SELECT_MODE_SINGLE)]],
+                   key='box container', pad=(0, 0), visible=False))],
     [
         sg.Text("Graphics type:"),
         sg.Combo(
@@ -141,76 +157,142 @@ layout = [
     [sg.Image(key="pokemon img"), sg.Text("", key="pokemon not found")],
 ]
 
-window = sg.Window("Pokemon", layout, size=(500, 650), finalize=True)
+window = sg.Window("Pokemon", layout, size=(550, 750), finalize=True, return_keyboard_events=True)
 window["pokemon name"].bind("<Return>", "_Enter")
+
+list_element: sg.Listbox = window.Element('box')
+prediction_list, input_text, sel_item = [], "", 0
+
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "Cancel":
         break
-
-    # Make pokemon image
-    if event == "OK" or event == "pokemon name" + "_Enter":
-        if not os.path.exists('pokemons'):
-            os.makedirs('pokemons')
-
-        # Check Pokémon image exists, if not create it
-        pokemon_data = pokemon_find(values['pokemon name'], values['graphic'], values['shiny'], values['who'])
-
-        # Correct data
-        if os.path.exists(f'pokemons/{pokemon_data[1]}.png'):
-            window["pokemon img"].update(filename=f'pokemons/{pokemon_data[1]}.png')
-            window["pokemon not found"].update("")
-            window["pokemon data"].update(
-                f"Name: {pokemon_data[0][0]}, ID: {pokemon_data[0][1]}"
-            )
-            window["Save"].update(disabled=False)
-            window["Previous"].update(disabled=False)
-            window["Next"].update(disabled=False)
-
-        # Incorrect data
+    elif event.startswith('Escape'):
+        window['pokemon name'].update('')
+        window['box container'].update(visible=False)
+    elif event.startswith('Down') and len(prediction_list):
+        sel_item = (sel_item + 1) % len(prediction_list)
+        list_element.update(set_to_index=sel_item, scroll_to_index=sel_item)
+    elif event.startswith('Up') and len(prediction_list):
+        sel_item = (sel_item + (len(prediction_list) - 1)) % len(prediction_list)
+        list_element.update(set_to_index=sel_item, scroll_to_index=sel_item)
+    elif event == '\r':
+        if len(values['box']) > 0:
+            window['pokemon name'].update(value=values['box'][0])
+            window['box container'].update(visible=False)
+    elif event == 'pokemon name':
+        text = values['pokemon name'].lower()
+        if text == input_text:
+            continue
         else:
+            input_text = text
+        prediction_list = []
+        if text:
+            prediction_list = [item for item in choices if item.lower().startswith(text)]
+
+        list_element.update(values=prediction_list)
+        sel_item = 0
+        list_element.update(set_to_index=sel_item)
+
+        if len(prediction_list) > 0:
+            window['box container'].update(visible=True)
+        else:
+            window['box container'].update(visible=False)
+    elif event == 'box':
+        window['pokemon name'].update(value=values['box'][0])
+        window['box container'].update(visible=False)
+
+#
+    # Make .pokemons.json in pokemons directory
+    if not os.path.exists('pokemons/.pokemons.json'):
+        pokemons_dict = requests.get('https://pokeapi.co/api/v2/pokemon/?limit=99999')
+        pokemons_dict = pokemons_dict.json()
+        pokemons_list = []
+        with open('pokemons/.pokemons.json', 'x') as pokemons_json:
+            for pokemon in pokemons_dict['results']:
+                pokemon_id = re.findall('/[0-9]+/', pokemon['url'])
+                pokemons_list.append({'name': f'{pokemon['name']}', 'id': pokemon_id[0].replace('/', '')})
+            json.dump(pokemons_list, pokemons_json, indent=4)
+
+    with open('pokemons/.pokemons.json', 'r') as pokemons_json:
+        pokemons_dict = json.load(pokemons_json)
+
+        try:
+            if event == "OK" or (event == "pokemon name" + "_Enter" and values['pokemon name'] == values['box'][0]):
+                # Make pokemons directory
+                if not os.path.exists('pokemons'):
+                    os.makedirs('pokemons')
+
+                # Check Pokémon exists in .pokemons.json
+                for pokemon_dict in pokemons_dict:
+                    if values['pokemon name'] == pokemon_dict['name'] or values['pokemon name'] == pokemon_dict['id']:
+                        # Check Pokémon image exists, if not create it
+                        pokemon_data = pokemon_find(values['pokemon name'], values['graphic'], values['shiny'], values['who'])
+
+                try:
+                    pokemon_data
+                except NameError:
+                    pokemon_data = [False, False]
+
+                print(pokemon_data)
+                # Correct data
+                if os.path.exists(f'pokemons/{pokemon_data[1]}.png'):
+                    window["pokemon img"].update(filename=f'pokemons/{pokemon_data[1]}.png')
+                    window["pokemon not found"].update("")
+                    window["pokemon data"].update(
+                        f"Name: {pokemon_data[0][0]}, ID: {pokemon_data[0][1]}"
+                    )
+                    window["Save"].update(disabled=False)
+                    window["Previous"].update(disabled=False)
+                    window["Next"].update(disabled=False)
+
+                # Incorrect data
+                else:
+                    window["pokemon not found"].update("Pokemon not found")
+                    window["pokemon img"].update()
+
+                # Save
+                if event == "Save" and os.path.exists(f'pokemons/{pokemon_data[1]}.png'):
+                    desktop = os.path.join(
+                        "c:\\Users", os.getlogin(), f"Desktop\\{pokemon_data[1]}.png"
+                    )
+                    file_path = sg.popup_get_file(
+                        "Path", save_as=True, default_path=desktop, default_extension=".png"
+                    )
+                    pokemon = Image.open(f'pokemons/{pokemon_data[1]}.png')
+                    if file_path is not None:
+                        pokemon.save(file_path)
+
+            # Previous Next
+            if event in ["Previous", "Next"] and os.path.exists(f'pokemons/{pokemon_data[1]}.png'):
+                pokemon = {'name': pokemon_data[0][0], 'id': str(pokemon_data[0][1])}
+                if event == 'Previous':
+                    pokemon_id = pokemons_dict[pokemons_dict.index(pokemon) - 1]['id']
+                else:
+                    try:
+                        pokemon_id = pokemons_dict[pokemons_dict.index(pokemon) + 1]['id']
+                    except IndexError:
+                        pokemon_id = 1
+
+                # Make new pokemon_data
+                pokemon_data = pokemon_find(str(pokemon_id), values['graphic'], values['shiny'], values['who'])
+
+            # Make new Pokémon.png
+                if os.path.exists(f'pokemons/{pokemon_data[1]}.png'):
+                    window["pokemon img"].update(filename=f'pokemons/{pokemon_data[1]}.png')
+                    window["pokemon not found"].update("")
+                    window["pokemon data"].update(f"Name: {pokemon_data[0][0]}, ID: {pokemon_data[0][1]}")
+
+                    # Change value "Pokémon name" with ID
+                    if values["pokemon name"].isdigit():
+                        window["pokemon name"].update(pokemon_data[0][1])
+
+                    # Change value "Pokémon name" with name
+                    else:
+                        window["pokemon name"].update(pokemon_data[0][0])
+        except TypeError:
             window["pokemon not found"].update("Pokemon not found")
             window["pokemon img"].update()
-
-    # Save
-    if event == "Save" and os.path.exists(f'pokemons/{pokemon_data[1]}.png'):
-        desktop = os.path.join(
-            "c:\\Users", os.getlogin(), f"Desktop\\{pokemon_data[1]}.png"
-        )
-        file_path = sg.popup_get_file(
-            "Path", save_as=True, default_path=desktop, default_extension=".png"
-        )
-        pokemon = Image.open(f'pokemons/{pokemon_data[1]}.png')
-        if file_path is not None:
-            pokemon.save(file_path)
-
-    # Previous Next
-    if event in ["Previous", "Next"] and os.path.exists(f'pokemons/{pokemon_data[1]}.png'):
-        pokemon_id = pokemon_data[0][1] + (-1 if event == "Previous" else 1)
-
-        # If it is the first Pokémon, jump to the last Pokémon using the previous button
-        if pokemon_id == 0:
-            pokemon_id = 1025
-        # If it is the last Pokémon, jump to the first Pokémon using the previous button
-        elif pokemon_id == 1026:
-            pokemon_id = 1
-
-        # Make new pokemon_data
-        pokemon_data = pokemon_find(str(pokemon_id), values['graphic'], values['shiny'], values['who'])
-
-        # Make new Pokémon.png
-        if os.path.exists(f'pokemons/{pokemon_data[1]}.png'):
-            window["pokemon img"].update(filename=f'pokemons/{pokemon_data[1]}.png')
-            window["pokemon not found"].update("")
-            window["pokemon data"].update(f"Name: {pokemon_data[0][0]}, ID: {pokemon_data[0][1]}")
-
-            # Change value "Pokémon name" with ID
-            if values["pokemon name"].isdigit():
-                window["pokemon name"].update(pokemon_data[0][1])
-
-            # Change value "Pokémon name" with name
-            else:
-                window["pokemon name"].update(pokemon_data[0][0])
 
 window.close()
